aaa787573fb7514fe2610415c0a28af93a3ab8e1
Tim Licata <tim@timothylicata.com>
2014-08-20 22:29:10 -0700

    posts accept tags and print them out

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,12 +4,23 @@ class PostsController < ApplicationController
   end
   def show
     @post = Post.find_by_id(params[:id])
+    @tags = @post.tags
   end
   def new
   end
   def create
-    form_data = params[:post].permit(:author, :title, :description)
-    Post.create(form_data)
+    post_data = params[:post].permit(:author, :title, :description)
+    tag_data = params[:tags].split(",").map(&:strip).map(&:downcase)
+
+    post = Post.create(post_data)
+    tag_data.each do |tag_str|
+      tag = Tag.find_by_name(tag_str)
+      if tag == nil
+        tag = Tag.create(:name => tag_str)
+      end
+      post.tags << tag
+    end
+
     redirect_to "/"
   end
 end
