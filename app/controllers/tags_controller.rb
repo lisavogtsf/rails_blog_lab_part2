2d11d3092483be9e74c07c143c26334f71dad4ef
Tim Licata <tim@timothylicata.com>
2014-08-21 10:04:28 -0700

    only show tags that have associated posts

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,8 @@
 class TagsController < ApplicationController
   def index
-    @all_tags = Tag.all
+    @all_tags = Tag.all.select do |tag|
+      tag.posts.length > 0
+    end
   end
   def show
     @tag = Tag.find_by_id(params[:id])
