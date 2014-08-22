63e6c15982210436d22d17265376fbe067408c39
Lisa Vogt <lisavogt.sf@gmail.com>
2014-08-21 22:05:14 -0700

    creates nested routes for comments within posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,70 +1,38 @@
 Rails.application.routes.draw do
 
-  root 'posts#index'
+	root 'posts#index'
 
-  get 'posts', to: 'posts#index'
-  get 'posts/new', to: 'posts#new'
-  get 'posts/:id', to: 'posts#show'
-  get 'posts/:id/edit', to: 'posts#edit'
-  get 'tags', to: 'tags#index'
-  get 'tags/:id', to: 'tags#show'
+	resources :posts do
+		resources :comments
+	end
 
-  post 'posts/create', to: 'posts#create'
-  delete 'posts/:id', to: 'posts#delete'
-  put 'posts/:id', to: 'posts#update'
+	get 'tags', to: 'tags#index'
+	get 'tags/:id', to: 'tags#show'
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+	#get 'posts', to: 'posts#index'
+	#get 'posts/new', to: 'posts#new'
+	#get 'posts/:id', to: 'posts#show'
+	#get 'posts/:id/edit', to: 'posts#edit'
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+	#post 'posts/create', to: 'posts#create'
+	#delete 'posts/:id', to: 'posts#destroy'
+	#put 'posts/:id', to: 'posts#update'
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+		# get '/posts', to: 'posts#index', as: 'posts'
 
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
+		# get '/posts/new', to: 'posts#new', as: 'new_creature'
 
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
+		# get '/posts/:id', to: 'posts#show', as: 'creature'
 
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
+		# get '/posts/:id/edit', to: 'posts#edit', as: 'edit_creature'
 
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
+		# post "/posts", to: "posts#create"
 
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+		# patch '/posts/:id', to: 'posts#update'
+
+		# delete "/posts/:id", to: "posts#destroy"
+
+
 end
